de83cf399ecf6599c2e190469d970effe3881a27
praveenkumar036 <praveenkumar821115@gmail.com>
2017-06-09 21:42:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+﻿
+var mainApp = angular.module("mainApp",['ngRoute']);
+
+mainApp.config(function($routeProvider){
+	
+	$routeProvider
+	.when('/home',{ templateUrl : 'home.html', 
+	                controller : 'studentController'})
+	.when('/viewStudents',{templateUrl: 'viewStudents.html',
+                           controller: 'studentController'})
+  .otherwise({redirectTo: '/home'})
+});
+
+mainApp.controller('studentController',function($scope){
+	
+	$scope.students ={
+		college:"BNM",
+		details:[
+		{name : 'Sachin tendulkar',city:'Mumbai'},
+		{name: 'Mark Waugh', city:'New York'},
+		{name: 'Steve Jonathan', city:'London'},
+		{name: 'John Marcus', city:'Paris'},
+	]};
+	
+	
+   $scope.message = "Click on the hyper link to view the students list.";
+	
+})
